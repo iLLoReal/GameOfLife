e2cf1d6e246632f9a8f2928e4f9fff0a21899373
olivier <olivier.laffon@epitech.eu>
2021-04-27 11:02:01 +0200

    Added Map component test

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
 import Adapter from 'enzyme-adapter-react-17-updated';
-
 import App from '../components/App';
+import Map from '../components/Map';
 import Enzyme, { shallow } from 'enzyme';
 Enzyme.configure({ adapter: new Adapter() })
 
@@ -15,4 +15,12 @@ describe('(Component) App', () => {
   it ('displays "Basic component"', () => {
     expect(AppWrapper.text() === 'Basic component').toBe(true);
   })
-})
+});
+
+describe('(Component) Map', () => {
+  const MapWrapper = shallow(<Map />);
+
+  it('renders without exploding', () => {
+    expect(MapWrapper).toHaveLength(1);
+  })
+});
